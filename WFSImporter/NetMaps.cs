d5e0a30ef2d82d5641a08b90633fbb41fc3493ef
Jericho Masigan <jco.masigan@gmail.com>
2016-05-16 22:35:30 +1200

    Commit!

--- WFSImporter/NetMaps.cs
+++ WFSImporter/NetMaps.cs
@@ -1,5 +1,4 @@
 ﻿// (C) Copyright 2016 by Jericho Masigan 
-//
 using System;
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.ApplicationServices;
@@ -8,28 +7,12 @@ using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
 
 // This line is not mandatory, but improves loading performances
-[assembly: CommandClass(typeof(WFSImporter.MyCommands))]
+[assembly: CommandClass(typeof(WFSImporter.AutoCADcmd))]
 
 namespace WFSImporter
 {
-
-    // This class is instantiated by AutoCAD for each document when
-    // a command is called by the user the first time in the context
-    // of a given document. In other words, non static data in this class
-    // is implicitly per-document!
-    public class MyCommands
+    public class AutoCADcmd
     {
-        // The CommandMethod attribute can be applied to any public  member 
-        // function of any public class.
-        // The function should take no arguments and return nothing.
-        // If the method is an intance member then the enclosing class is 
-        // intantiated for each document. If the member is a static member then
-        // the enclosing class is NOT intantiated.
-        //
-        // NOTE: CommandMethod has overloads where you can provide helpid and
-        // context menu.
-
-        // Modal Command with localized name
         [CommandMethod("NETMAPS", CommandFlags.Modal)]
         public void NetMaps()
         {
